7a28e89f82b9669cea698ff37d291c8ea7cb5b23
Junyoung McArdle <juny.mcardle@gmail.com>
2019-05-19 03:53:42 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 2, "male", "brown", "clam", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "male", "brown", "clam", 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "male", "black", "rowdy", 1);
 
